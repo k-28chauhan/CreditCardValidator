1af10f4eba3df1b06955a554ae1aae96793185ae
k-28chauhan <65769385+k-28chauhan@users.noreply.github.com>
2024-09-19 00:01:30 +0530

    Update creditcardvalidator.cpp

--- creditcardvalidator.cpp
+++ creditcardvalidator.cpp
@@ -26,13 +26,13 @@ int main()
     return 0;
 }
 
-int getDigits(int number)
+int getDigits(int number)    //This function is used to return the sum of digits
 {
 
     return (number % 10) + (number/10 % 10);
     
 }
-int sumEvenDigits(string cardNumber){
+int sumEvenDigits(string cardNumber){    //This function is used to get the even digits and pass them to getDigits function
 
     int sum = 0;
     for(int i = cardNumber.size() - 2; i>=0; i-=2)
@@ -42,7 +42,7 @@ int sumEvenDigits(string cardNumber){
     return sum;
 
 }
-int sumOddDigits(string cardNumber){
+int sumOddDigits(string cardNumber){    //This function is used to get the Odd digits and pass them to getDigits function
 
     int sum = 0;
     for(int i = cardNumber.size() - 1; i>=0; i-=2)
